419a957c52c8e8301db70ab72e80c754558b710d
Poonam <35730395+Poonam0602@users.noreply.github.com>
2018-04-03 20:40:35 +0530

    thread added for receiving task

--- TCP_Socket_Programming/2wayChat_client.c
+++ TCP_Socket_Programming/2wayChat_client.c
@@ -5,13 +5,50 @@
 #include<string.h>    //strlen
 #include<sys/socket.h>    //socket
 #include<arpa/inet.h> //inet_addr
- 
+#include<pthread.h>
+#include<unistd.h>    //write
+#include<stdlib.h>
+
+#define MESSAGE_BUFFER 5000
+
+void * receive1(void * socket) 
+{
+    int socket_fd, response;
+    char message[MESSAGE_BUFFER];
+    //memset(message, 0, MESSAGE_BUFFER); // Clear message buffer
+    socket_fd = (int) socket;
+
+    // Print received message
+    while(1) 
+    {
+        memset(message, 0, MESSAGE_BUFFER); // Clear message buffer
+        response = recvfrom(socket_fd, message, MESSAGE_BUFFER, 0, NULL, NULL);
+        
+        if (response == -1) 
+        {
+          fprintf(stderr, "recv() failed\n");
+          break;
+        } 
+        else if (response == 0) 
+        {
+              printf("\nPeer disconnected\n");
+              break;
+        } 
+        else 
+        {
+              printf("\n>>%s", message);
+              fflush(stdout); 
+        }
+    }
+}
+
+
 int main(int argc , char *argv[])
 {
     int sock;
     struct sockaddr_in server;
-    char message[1000] , server_reply[2000];
-     
+    char message[MESSAGE_BUFFER];
+    pthread_t receive;
     //Create socket
     sock = socket(AF_INET , SOCK_STREAM , 0);
     if (sock == -1)
@@ -33,32 +70,16 @@ int main(int argc , char *argv[])
      
     puts("Connected\n");
      
-    //keep communicating with server
-    while(1)
-    {        
-	memset(server_reply, '\0', sizeof(server_reply));	
-	printf("\nEnter message : ");
-        //scanf("%s" , message);
-	gets(message);
-         
-        //Send some data
-        if( send(sock , message , strlen(message) , 0) < 0)
+    pthread_create(&receive, NULL, receive1, (void *) sock);
+    while (fgets(message, MESSAGE_BUFFER, stdin) != NULL) 
+    {
+        if (strncmp(message, "/q", 2) == 0) 
         {
-            puts("Send failed");
-            return 1;
+            printf("Closing connection...\n");
+            exit(0);
         }
-         
-        //Receive a reply from the server
-        if( recv(sock , server_reply , 2000 , 0) < 0)
-        {
-            puts("recv failed");
-            break;
-        }
-         
-        puts("\nServer reply :");
-        printf("%s",server_reply);
+        write(sock , message , strlen(message));
     }
-     
     close(sock);
     return 0;
 }
